oktetlabs/te-jenkins

commit c6f6d7d7f4c8d2c51680fb51111d64314ed0d4ed

vars: implement a shared library
Share CI best practices for TE-based test suites.
This library allows to avoid corresponding logic duplication and
simplifies addition of Jenkins support to test suites.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teTsPipeline.groovy
@@ -1,0 +1,441 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Pipeline template for running tests.
+//
+// Required plugins:
+//   - Pipeline: Stage Step
+//   - Pipeline: SCM step
+//   - Timestamper
+//   - Email Extension
+//   - Copy Artifact
+//   - JUnit
+//
+// Pipeline does:
+//   1. Call preStartHook if needed (see "Available pipeline hooks")
+//   2. Clone TE/TS/TS Conf/TS Rigs with revisions that you define
+//   3. Call preRunHook if needed (see "Available pipeline hooks")
+//   4. Start run.sh with options that you define (see optionProvider)
+//   5. Call postRunHook if needed (see "Available pipeline hooks")
+//   6. Save artifacts and post JUnit log
+//   7. Trigger downstream jobs if needed
+//   8. Generate meta_data.json in always
+//   9. Call postAlwaysHook if needed (see "Available pipeline hooks")
+//   10. Post email about status of build
+//
+// Default job parametes
+// Note: tsconf_* parameters are present only if tsconf template parameter
+// was set to true.
+//
+//   te_rev: TE revision, can be tag, branch or SHA-1
+//   te_repo: TE repository (can also be set in TE_GIT_URL environment
+//            variable in Jenkins; value of this parameter can
+//            override the environment)
+//   ts_rev: TS revision, can be tag, branch or SHA-1
+//   ts_repo: test suite repository (can also be set in TS_GIT_URL
+//            environment variable in Jenkins; value of this parameter
+//            can override the environment)
+//   tsconf_rev: TS Conf revision, can be tag, branch or SHA-1
+//   tsconf_repo: ts-conf repository (can also be set in TSCONF_GIT_URL
+//                environment variable in Jenkins; value of this parameter
+//                can override the environment)
+//   tsrigs_rev: TS rigs revision, can be tag, branch or SHA-1
+//   tsrigs_repo: ts-rigs repository (can also be set in TSRIGS_GIT_URL
+//                environment variable in Jenkins; value of this
+//                parameter can override the environment)
+//   ts_opts: Additional options for run.sh as string
+//   with_tce: Boolean to toggle --tce option for run.sh
+//   downstream_jobs: Comma-separated list of jobs to be triggered
+//                    in case of success (sticky default)
+//   update_job: Job from which to get revisions of the last
+//               successful build (sticky default)
+//   ts_cfg: Tested configuration name (sticky default)
+//   lock: Lock to acquire and hold while running. Empty by default
+//         which means that ts_cfg should be used as a lock name.
+//         (opaque string, sticky default)
+//
+// Additional parameters present if sticky_repo_params template parameter
+// was set:
+//   te_branch: TE branch to use (may be overridden by te_rev or TE_REV;
+//              sticky default)
+//   ts_branch: TS branch to use (may be overridden by ts_rev or TS_REV;
+//              sticky default)
+//   tsrigs_branch: ts-rigs branch (may be overridden by tsrigs_rev or
+//                  TSRIGS_REV; sticky default)
+//   tsconf_branch: ts-conf branch (may be overridden by tsconf_rev or
+//                  TSCONF_REV; sticky default)
+//
+// Available parameters of pipeline template:
+//   ts_name: Test suite name.
+//   label: String. Execute the Pipeline on an agent available in the Jenkins
+//          environment with the provided label. Required.
+//   specificParameters: List of additional job parameters which can be
+//                       used in test suite specific hooks. Optional.
+//   optionsProvider: Closure that returns list of options for run.sh. Required.
+//   triggersProvider: Closure that returns list of conditions when job starts
+//                     automatically. Optional.
+//   tsconf: if true, it is required to clone TS Conf repository (not all
+//           test suites may need it)
+//   sticky_repo_params: if true, sticky [te|ts|tsconf|tsrigs]_branch
+//                       parameters are added and *_repo parameters
+//                       become sticky too.
+//   publish_logs: if true, in post/always section trigger publish-log
+//                 job to copy logs to a specified location and bublik-import
+//                 job to export logs to Bublik web application.
+//
+// Available pipeline hooks (see "Pipeline does" for understanding when hook
+// is called):
+//   preStartHook: Closure is called before TE/TS/TS Conf/TS Rigs checkout.
+//                 Useful for overriding the te/ts/tsconf/tsrigs revisions or
+//                 checkout of your tools. Optional parameter.
+//                 Pre-start hook must not be used for any kind of operations
+//                 which require the resource to be locked (e.g. reboot hosts,
+//                 install required SW, configure it etc).
+//   preRunHook: Closure is called after TE/TS/TS Conf/TS rigs checkout and
+//               before starting run.sh. Useful for preparing your specific
+//               environment and tools before running test suite. Optional
+//               parameter.
+//   postRunHook: Closure is called after run.sh and before saving artifacts.
+//                Useful for archiving your specific artifacts, cleanup.
+//                Optional parameter.
+//   postAlwaysHook: Closure is called at post stage. Usefull for E-mail etc.
+//                   Optional parameter.
+//   postUnsuccessfulHook: Closure is called at post stage. Usefull for E-mail etc.
+//                   Optional parameter.
+//   postCleanupHook: Closure is called at the end of post stage. Usefull for E-mail etc.
+//                   Optional parameter.
+//
+// See "Pipeline templates" in jenkins/README.md for more information on
+// how to use this template.
+
+def call(Closure body) {
+    def ctx = teCommon.create_delegate(env, params)
+    def emailRecipientProviders = []
+
+    // DELEGATE_FIRST means that the delegate is used firstly
+    // to resolve properties. So that any property set in closure
+    // body will be set in ctx.
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = ctx
+    body()
+
+    pipeline {
+
+        agent { label ctx.label }
+
+        options {
+            buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '100'))
+            timestamps()
+            copyArtifactPermission('*')
+            checkoutToSubdirectory('te-jenkins')
+
+            // Concurrent builds are enabled to make it possible to define
+            // the single pipeline and use it for running testing on
+            // multiple hosts simultaneously. Lock at run stage should
+            // be used to prevent simultaneous access to the same resource.
+        }
+
+        stages {
+            stage('Job prepare') {
+                steps {
+                    echo "Workspace: ${env.WORKSPACE}"
+                    echo "Job: ${env.JOB_NAME}"
+                    echo "Host: ${env.NODE_NAME}"
+
+                    script {
+                        currentBuild.displayName = "#${BUILD_NUMBER}"
+                        if (params.ts_cfg) {
+                            currentBuild.displayName += " (${ts_cfg})"
+                        }
+                    }
+
+                    script {
+                        def triggersList = []
+                        def paramsList = [
+                            string(name: 'ts_opts',
+                                   defaultValue: '',
+                                   description: 'Additional options for run.sh'),
+                            booleanParam(name: 'with_tce',
+                                         defaultValue: false,
+                                         description: 'Add --tce options for run.sh'),
+
+                            string(name: 'ts_cfg',
+                                   defaultValue: params.ts_cfg,
+                                   description: 'Name of configuration (sticky default)'),
+
+                            string(name: 'lock',
+                                   defaultValue: params.lock,
+                                   description: 'Lock to acquire (sticky default)'),
+
+                           string(name: 'downstream_jobs',
+                                   defaultValue: params.downstream_jobs,
+                                   description: 'List of downstream jobs (sticky default)'),
+
+                           string(name: 'update_job',
+                                   defaultValue: params.update_job,
+                                   description: 'Update job (sticky default)'),
+                        ]
+
+                        paramsList.addAll(teRun.get_repo_params(
+                                                      params, ctx))
+
+                        if (ctx.containsKey('specificParameters')) {
+                            paramsList += ctx['specificParameters']
+                        }
+
+                        if (ctx.containsKey('triggersProvider')) {
+                            triggersList +=
+                                ctx.triggersProvider()
+                        }
+
+                        properties([
+                            parameters(paramsList),
+                            pipelineTriggers(triggersList),
+                        ])
+                    }
+
+                    script {
+                        // Do cleanup as the first action to ensure that
+                        // previous run results are not saved as artifacts
+                        teRun.cleanup()
+
+                        teEmail.email_start()
+                    }
+                }
+            }
+
+            stage('Pre start') {
+                steps {
+                    script {
+                        teEmail.email_stage('Pre start')
+
+                        ctx.metas.TS_NAME = ctx.ts_name ?: ""
+                        ctx.metas.CFG = params.ts_cfg ?: ""
+
+                        ctx.metas.TCE = params.with_tce ? 'true' : 'false'
+
+                        def date_str
+                        if (env.CAMPAIGN_DATE_OFFSET) {
+                            date_str = env.CAMPAIGN_DATE_OFFSET
+                        } else {
+                            // By default we add 6 hours so that testing
+                            // sessions started before midnight have the
+                            // same date as testing sessions started after
+                            // midnight on the same evening.
+                            date_str = "+6 hours"
+                        }
+                        ctx.metas.CAMPAIGN_DATE = sh(
+                              script: "date --date=\"${date_str}\" +%F",
+                              returnStdout: true).trim()
+
+                        ctx.metas.START_TIMESTAMP =
+                                          teMeta.meta_timestamp_make()
+                        ctx.metas.RUN_OK = "false"
+                        ctx.metas.RUN_STATUS = "ERROR"
+
+                        if (params.update_job) {
+                            ctx.all_revs =
+                                teRevData.try_load_revs_from_job(
+                                              params.update_job)
+                        }
+
+                        if (ctx.containsKey('preStartHook')) {
+                            ctx.preStartHook()
+                        }
+
+                        teRevData.export_values(env, ctx.all_revs, true)
+                    }
+                }
+            }
+
+            stage('Clone ts-rigs sources') {
+                when { expression { env.TSRIGS_GIT_URL } }
+                steps {
+                    script {
+                        teEmail.email_stage('Clone ts-rigs sources')
+                        teRun.tsrigs_checkout(ctx)
+
+                        teRun.tsrigs_load(ctx)
+                    }
+                }
+            }
+
+            stage('Clone TE and TS sources') {
+                steps {
+                    script {
+                        teEmail.email_stage('Clone TE/TS sources')
+
+                        teRun.te_checkout(ctx)
+                        teRun.ts_checkout(ctx)
+                    }
+                }
+            }
+
+            stage('Clone ts-conf sources') {
+                when { expression { ctx.tsconf } }
+                steps {
+                    script {
+                        teEmail.email_stage('Clone ts-conf sources')
+                        teRun.tsconf_checkout(ctx)
+                    }
+                }
+            }
+
+            stage('Run') {
+                steps {
+                    lock(params.lock ?: params.ts_cfg) {
+                        script {
+
+                            // Bublik needs PROJECT to be present in metadata
+                            // for sanity check. Do it here because
+                            // ctx.PROJECT may be defined in ts-rigs.
+                            ctx.metas.PROJECT = ctx.PROJECT ?: ctx.ts_name ?: ""
+
+                            teRun.define_logs_paths(ctx)
+
+                            teEmail.email_stage('Run')
+
+                            // Prepare initial meta_data.json for live logs
+                            // listener.
+                            if (ctx.TS_LOG_LISTENER_NAME) {
+                                // Temporarily set status to running to
+                                // have it in live import metadata
+                                ctx.metas.RUN_STATUS = "RUNNING"
+                                teMeta.meta_generate(ctx)
+                                ctx.metas.RUN_STATUS = "ERROR"
+                            }
+
+                            if (ctx.containsKey('preRunHook')) {
+                                ctx.preRunHook()
+                            }
+
+                            def opts = ctx.optionsProvider()
+                            def cfg = params.ts_cfg ?: ""
+                            if (params.with_tce) {
+                                opts.add('--tce')
+                            }
+                            if (ctx.TS_LOG_LISTENER_NAME) {
+                                opts.add('--logger-listener=' +
+                                         ctx.TS_LOG_LISTENER_NAME)
+                                opts.add('--logger-meta-file=' +
+                                         teMeta.meta_data_fpath())
+                            }
+                            opts.add(params.ts_opts)
+                            if (env.HTML_LOGS) {
+                                opts.add("--trc-html-logs=${env.HTML_LOGS}html")
+                            }
+
+                            teRun.run(ctx, cfg, opts)
+                            ctx.metas.RUN_OK = "true"
+                            ctx.metas.RUN_STATUS = "DONE"
+
+                            ctx.metas.FINISH_TIMESTAMP =
+                                                teMeta.meta_timestamp_make()
+                            if (ctx.containsKey('postRunHook')) {
+                                ctx.postRunHook()
+                            }
+                        }
+                    }
+                }
+            }
+
+            stage('Save statistics') {
+                steps {
+                    script {
+                        teEmail.email_stage('Save statistics')
+
+                        ctx.ts_stats = teRun.statistics()
+                    }
+                }
+            }
+
+        }
+
+        post {
+            always {
+                script {
+                    teEmail.email_all_revs(ctx.all_revs)
+
+                    if (ctx.ts_stats != null) {
+                        def total = ctx.ts_stats.totalCount
+                        def failed = ctx.ts_stats.failCount
+                        def skipped = ctx.ts_stats.skipCount
+                        def passed = total - failed - skipped
+
+                        if (total > 0) {
+                            teEmail.email_set_trailer("${passed}+${failed}+" +
+                                                    "${skipped}=${total}")
+                        }
+                    }
+
+                    teRevData.save_revs(ctx.all_revs)
+                    archiveArtifacts artifacts: 'all.rev'
+
+                    if (ctx.metas.FINISH_TIMESTAMP == null) {
+                        ctx.metas.FINISH_TIMESTAMP =
+                                        teMeta.meta_timestamp_make()
+                    }
+
+                    teMeta.meta_generate(ctx)
+                    archiveArtifacts artifacts: teMeta.meta_data_fname()
+
+                    try {
+                        // We want to try archive things for investigation
+                        teRun.strip()
+                        teRun.archive()
+                    } catch(Exception e) {
+                        // Ignore error
+                    }
+
+                    if (ctx.publish_logs && ctx.LOGS_PATH) {
+                        teRun.publish_logs(ctx)
+
+                        if (env.HTML_LOGS) {
+                            teEmail.email_newline()
+                            teEmail.email_message("Logs archive:")
+                            teEmail.email_message("${env.HTML_LOGS}")
+                        }
+                    }
+
+                    teEmail.email_newline()
+                    teEmail.email_message("Parameters:")
+                    params.sort().each {
+                        name, value ->
+                        teEmail.email_message("${name}=${value}")
+                    }
+
+                    if (ctx.containsKey('postAlwaysHook')) {
+                        ctx.postAlwaysHook()
+                    }
+                }
+            }
+            success {
+                script {
+                    teEmail.email_post(true, emailRecipientProviders)
+
+                    if (params.downstream_jobs) {
+                        params.downstream_jobs.tokenize(',').each {
+                            job_name ->
+                            build(job: job_name, wait: false)
+                        }
+                    }
+                }
+            }
+            unsuccessful {
+                script {
+                    teEmail.email_post(false, emailRecipientProviders)
+                    if (ctx.containsKey('postUnsuccessfulHook')) {
+                        ctx.postUnsuccessfulHook()
+                    }
+                }
+            }
+            cleanup {
+                script {
+                    if (ctx.containsKey('postCleanupHook')) {
+                        ctx.postCleanupHook()
+                    }
+                }
+            }
+        }
+    }
+}

vars/teCommon.groovy
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Generic helper functions.
+
+// Create delegate map for a closure which is passed to pipeline
+// templates.
+//
+// Return:
+//   Map to use as a delegate
+def create_delegate(env, params) {
+    def dlg = [:]
+
+    // Using DELEGATE_FIRST makes parts of this library not
+    // available inside the closure unless we set references
+    // to them in the delegate object.
+
+    dlg.teRun = teRun
+    dlg.teEmail = teEmail
+    dlg.teMeta = teMeta
+    dlg.teRevData = teRevData
+    dlg.teCommon = teCommon
+
+    dlg.env = env
+    dlg.params = params
+
+    // Having reference to this delegate itself makes it
+    // simpler to use some API methods
+    dlg.teCtx = dlg
+
+    dlg.metas = [:]
+    dlg.all_revs = [:]
+
+    return dlg
+}

vars/teEmail.groovy
@@ -1,0 +1,152 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for email generation.
+
+// Set email sender.
+//
+// Args:
+//   address: email address
+def email_set_from(String address) {
+    env.EMAIL_FROM = address
+}
+
+// Add email recipient.
+//
+// Args:
+//   address: email address
+def email_add_to(String address) {
+    if (env.EMAIL_TO == null) {
+        env.EMAIL_TO = ""
+    }
+    env.EMAIL_TO += "${address};"
+}
+
+// Set prefix in email subject.
+// By default it is "[CI <jobname>]".
+//
+// Args:
+//   prefix: prefix string
+def email_set_prefix(String prefix) {
+    env.EMAIL_PREFIX = prefix
+}
+
+// Set trailer in email subject.
+//
+// Args:
+//   trailer: trailer string
+def email_set_trailer(String trailer) {
+    env.EMAIL_TRAILER = trailer
+}
+
+// Get path to a file where email message text is constructed.
+//
+// Return:
+//   Email file path relative to WORKSPACE
+def email_file_get() {
+    return "${env.WORKSPACE}/email.txt"
+}
+
+// Reset email message: make it clean and print basic job info
+def email_start() {
+    def email_file = email_file_get()
+    sh "echo > ${email_file}"
+
+    email_variable_message('WORKSPACE', env.WORKSPACE)
+    email_variable_message('JOB_NAME', env.JOB_NAME)
+    email_variable_message('NODE_NAME', env.NODE_NAME)
+    email_newline()
+    email_message("Build URL: ${env.BUILD_URL}")
+    email_message("Timestamp: ${env.BUILD_TIMESTAMP}")
+    email_newline()
+}
+
+// Add message to email
+//
+// Args:
+//   message: message string
+def email_message(String message) {
+    def email_file = email_file_get()
+    sh "echo '${message}' >> ${email_file}"
+}
+
+// Just wrapper for adding empty line
+def email_newline() {
+    email_message("")
+}
+
+// Add variable to email
+//
+// Args:
+//   name: name of variable
+//   value: value of variable
+def email_variable_message(String name, String value) {
+    email_message("${name}=${value}")
+}
+
+// Add all revisions to email text. Revision are taken
+// from the map constructed with help of teRevData.
+//
+// Args:
+//   revs: map with revisions
+def email_all_revs(Map revs) {
+    def revs_only = []
+
+    teRevData.iterate_values(revs, {
+        component, variable, value ->
+        if (variable ==~ /.*_REV$/) {
+            revs_only.add("${variable}=${value}")
+        }
+    })
+
+    email_newline()
+    email_message("Revisions:")
+    revs_only.sort().each {
+        value ->
+        email_message(value)
+    }
+}
+
+// Add stage to email
+//
+// Args:
+//   stage: stage name
+def email_stage(String stage) {
+    email_message("STAGE: ${stage}")
+}
+
+// Post email by status
+//
+// Args:
+//   status: status of build
+//   providers: recipient providers, i.e. requestor() or culprits() etc...
+def email_post(status, providers) {
+    def email_file = email_file_get()
+    def prefix = env.EMAIL_PREFIX ?: "[CI ${env.JOB_NAME}]"
+    def subject
+
+    email_newline()
+    if (status) {
+        email_message("OK")
+    } else {
+        email_message("FAIL")
+    }
+    email_newline()
+
+    subject = "${prefix} job ${currentBuild.displayName}: "
+    subject += "${currentBuild.currentResult}"
+
+    if (env.EMAIL_TRAILER) {
+        subject += " ${env.EMAIL_TRAILER}"
+    }
+
+    emailext (
+        subject: "${subject}",
+        to: env.EMAIL_TO,
+        from: env.EMAIL_FROM,
+        recipientProviders: providers,
+        attachLog: true,
+        attachmentsPattern: '**/trc-brief.html',
+        body: '${FILE, path="' + email_file + '"}'
+    )
+}

vars/teMeta.groovy
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for generating meta_data.json which describes the run
+// and is used by Bublik.
+//
+// Requires:
+//     Pipeline Utility Steps
+
+// Get name of the file with metas
+def meta_data_fname() {
+    return 'meta_data.json'
+}
+
+// Get path to the file with metas
+def meta_data_fpath() {
+    return env.WORKSPACE + '/' + meta_data_fname()
+}
+
+// Meta generation. It saves to JSON file all contents of metas
+// variable. You can set fields in that variable to add them to
+// meta data.
+//
+// Args:
+//     ctx: pipeline context containing metas
+def meta_generate(ctx) {
+    meta_data = [
+        version: 1,
+        metas: []
+    ]
+
+    ctx.metas.each { var_name, var_value ->
+        if (var_value == null) {
+            if (ctx.containsKey(var_name)) {
+                var_value = ctx[var_name]
+            } else {
+                var_value = env[var_name]
+            }
+        }
+        if (var_value == null) {
+            return
+        }
+
+        meta_node = [
+            name: var_name,
+            value: var_value
+        ]
+
+        def meta_type = meta_name2type(var_name)
+        if (meta_type != null) {
+            meta_node.type = meta_type
+        }
+
+        meta_data.metas.add(meta_node)
+    }
+
+    writeJSON(file: meta_data_fpath(), json: meta_data, pretty: 4)
+}
+
+// Get meta node type by tail of node name
+//
+// Args:
+//     node_name: Name of node, for example TE_REV, START_TIMESTAMP
+// Return:
+//     String with type of node or null
+def meta_name2type(node_name) {
+    if (node_name.endsWith('_TIMESTAMP')) {
+        return 'timestamp'
+    }
+    if (node_name.endsWith('_COMMIT') || node_name.endsWith('_REV')) {
+        return 'revision'
+    }
+    if (node_name.endsWith('_BRANCH')) {
+        return 'branch'
+    }
+
+    return null
+}
+
+// Get current timestamp
+//
+// Return:
+//    timestamp string
+def meta_timestamp_make() {
+    def timestamp = sh(script: 'date +%FT%T%:z', returnStdout: true).trim()
+    return timestamp
+}

vars/teRevData.groovy
@@ -1,0 +1,115 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for saving and loading information about revisions and
+// repositories.
+//
+// It allows to collect all information about revisions and repositories
+// in a single map, save it to a file in JSON format which can be
+// archived as an artifact. Then another job can copy that artifact,
+// load JSON file and export values from it in environment.
+
+// Load revisions map from a file.
+//
+// Args:
+//   fpath: Path to the JSON file
+//
+// Return:
+//   Loaded map (empty if the file is absent)
+def load_revs(String fpath = "all.rev") {
+    if (fileExists(fpath)) {
+        return readJSON(file: fpath)
+    } else {
+        return [:]
+    }
+}
+
+// Try to get map with revisions from an artifact of the last successful
+// build of a given job.
+//
+// Args:
+//   job: name of the job
+//   fpath: file to get from artifacts
+//
+// Return:
+//   Loaded map (empty if the file or job is absent)
+def try_load_revs_from_job(String job = "update",
+                           String fpath = "all.rev") {
+    try {
+        copyArtifacts(projectName: job,
+                      filter: fpath, selector: lastSuccessful())
+    } catch (Exception e) {
+        return [:]
+    }
+
+    return load_revs(fpath)
+}
+
+// Store named value in a map.
+//
+// Args:
+//   revs: map
+//   component: component name (for example, "te")
+//   name: value name (for example, "TE_REV")
+//   value: value to store
+def store_value(Map revs, String component, String name, String value) {
+    if (!revs[component]) {
+        revs[component] = [:]
+    }
+
+    revs[component][name] = value
+}
+
+// Export named values to environment.
+//
+// Args:
+//   env: Environment object
+//   revs: map with values
+//   force: if true, existing environment variables should be
+//          overwritten even if they have nonempty values
+//   component: component name (if null, all components are iterated)
+//   name: value name (if null, all values will be exported)
+def export_values(env, Map revs, boolean force = false,
+                  String component = null, String name = null) {
+    revs.each {
+        comp_name, comp_vars ->
+        if (!component || component == comp_name) {
+            comp_vars.each {
+                var_name, var_value ->
+
+                if (!name || name == var_name) {
+                    if (force) {
+                        env[var_name] = var_value
+                    } else {
+                        env[var_name] = env[var_name] ?:
+                                        var_value
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Iterate all values in revisions map.
+//
+// Args:
+//   revs: map to iterate
+//   it: closure taking three arguments - component name, variable name and
+//       value
+def iterate_values(Map revs, Closure it) {
+    revs.each {
+        comp_name, comp_vars ->
+        comp_vars.each {
+            var_name, var_value -> it(comp_name, var_name, var_value)
+        }
+    }
+}
+
+// Save map with revisions to a JSON file.
+//
+// Args:
+//   revs: map
+//   fpath: file path
+def save_revs(Map revs, String fpath = "all.rev") {
+    writeJSON(file: fpath, json: revs, pretty: 4)
+}

commit 9476fc1f040415b44551dddcb7e350aca826324a

doc: add missing plugin to lists
Lockable Resources plugin provides lock() used by teTsPipeline
template.

Fixes: c6f6d7d7f4c8 ("vars: implement a shared library")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teTsPipeline.groovy
@@ -10,6 +10,7 @@
 //   - Email Extension
 //   - Copy Artifact
 //   - JUnit
+//   - Lockable Resources
 //
 // Pipeline does:
 //   1. Call preStartHook if needed (see "Available pipeline hooks")