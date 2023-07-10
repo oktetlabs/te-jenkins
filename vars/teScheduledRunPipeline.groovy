oktetlabs/te-jenkins

commit c6f6d7d7f4c8d2c51680fb51111d64314ed0d4ed

vars: implement a shared library
Share CI best practices for TE-based test suites.
This library allows to avoid corresponding logic duplication and
simplifies addition of Jenkins support to test suites.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teScheduledRunPipeline.groovy
@@ -1,0 +1,139 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Pipeline template to run tests according to a schedule on
+// various hosts.
+//
+// Required plugins:
+//   - Pipeline: Stage Step
+//   - Timestamper
+//
+// Pipeline is expected to have the following mandatory parameters:
+//   job: which Jenkins job this pipeline should trigger
+//
+// It is assumed that the pipeline is triggered automatically
+// according to a schedule where it is specified at which time
+// which job should be run and with which parameters. All parameters
+// except job are passed to the triggered job.
+//
+// Template caller should set the following properties in a closure
+// passed to the template:
+//   label: label of the Jenkins agent
+//   schedule: parameterized schedule in the format accepted by
+//             parameterizedCron()
+//   specificParameters: list of test suite specific additional
+//                       parameters which will be passed to
+//                       the triggered job (optional)
+//
+// It is assumed that this template is used to define pipeline
+// in ts-rigs where information about testing configurations and
+// schedule of their usage in testing for specific test
+// suites is stored. For example, it can be used like
+//
+// scheduledRunPipeline {
+//     label = 'main'
+//
+//     schedule = '''
+// H 03 * * * % job=host1_run
+// '''
+//
+//     specificParameters = [
+//       string(name: 'ts_opts', defaultValue: '',
+//              description: 'Additional test suite options')
+//     ]
+// }
+//
+// See "Pipeline templates" in jenkins/README.md for more information on
+// how to use this template.
+
+def call(Closure body) {
+    def job_params = []
+    def ctx = teCommon.create_delegate(env, params)
+
+    def get_job_param = {
+        pname, pvalue ->
+
+        switch (pvalue.getClass()) {
+            case java.lang.String:
+                return string(name: pname, value: pvalue)
+
+            case java.lang.Boolean:
+                return booleanParam(name: pname, value: pvalue)
+        }
+    }
+
+    def get_job_params = {
+        params ->
+
+        params.each {
+            name, value ->
+            if (name != 'job') {
+                job_params.add(get_job_param(name, value))
+            }
+        }
+    }
+
+    // DELEGATE_FIRST means that the delegate is used firstly
+    // to resolve properties. So that any property set in closure
+    // body will be set in ctx.
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = ctx
+    body()
+
+    pipeline {
+
+        options {
+            buildDiscarder(logRotator(daysToKeepStr:'15', numToKeepStr:'100'))
+            timestamps()
+        }
+
+        agent {
+            label ctx.label
+        }
+
+        stages {
+
+            stage("Update schedule and parameters") {
+                steps {
+                    script {
+                        def params_list = [
+                            string(name: 'job', defaultValue: '',
+                                   description: 'Job to execute'),
+                        ]
+
+                        if (ctx.specificParameters) {
+                            params_list.addAll(
+                                  ctx.specificParameters)
+                        }
+
+                        // pollSCM() trigger is added here so that
+                        // schedule is updated automatically in Jenkins
+                        // after changes in sources.
+                        properties([parameters(params_list),
+                                    pipelineTriggers([
+                                        parameterizedCron(
+                                                  ctx.schedule),
+                                        pollSCM('H * * * *')])])
+                    }
+                }
+            }
+
+            stage("Trigger downstream job") {
+                steps {
+                    script {
+                        // This pipeline can be run without job argument
+                        // manually or when it is triggered by pollSCM().
+                        // It allows to update the schedule after sources
+                        // change.
+                        if (params.job) {
+                            get_job_params(params)
+
+                            build job: params.job, wait: false,
+                                       parameters: job_params
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

vars/teCommon.groovy
@@ -1,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Generic helper functions.
+
+// Create delegate map for a closure which is passed to pipeline
+// templates.
+//
+// Return:
+//   Map to use as a delegate
+def create_delegate(env, params) {
+    def dlg = [:]
+
+    // Using DELEGATE_FIRST makes parts of this library not
+    // available inside the closure unless we set references
+    // to them in the delegate object.
+
+    dlg.teRun = teRun
+    dlg.teEmail = teEmail
+    dlg.teMeta = teMeta
+    dlg.teRevData = teRevData
+    dlg.teCommon = teCommon
+
+    dlg.env = env
+    dlg.params = params
+
+    // Having reference to this delegate itself makes it
+    // simpler to use some API methods
+    dlg.teCtx = dlg
+
+    dlg.metas = [:]
+    dlg.all_revs = [:]
+
+    return dlg
+}

vars/teEmail.groovy
@@ -1,0 +1,152 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for email generation.
+
+// Set email sender.
+//
+// Args:
+//   address: email address
+def email_set_from(String address) {
+    env.EMAIL_FROM = address
+}
+
+// Add email recipient.
+//
+// Args:
+//   address: email address
+def email_add_to(String address) {
+    if (env.EMAIL_TO == null) {
+        env.EMAIL_TO = ""
+    }
+    env.EMAIL_TO += "${address};"
+}
+
+// Set prefix in email subject.
+// By default it is "[CI <jobname>]".
+//
+// Args:
+//   prefix: prefix string
+def email_set_prefix(String prefix) {
+    env.EMAIL_PREFIX = prefix
+}
+
+// Set trailer in email subject.
+//
+// Args:
+//   trailer: trailer string
+def email_set_trailer(String trailer) {
+    env.EMAIL_TRAILER = trailer
+}
+
+// Get path to a file where email message text is constructed.
+//
+// Return:
+//   Email file path relative to WORKSPACE
+def email_file_get() {
+    return "${env.WORKSPACE}/email.txt"
+}
+
+// Reset email message: make it clean and print basic job info
+def email_start() {
+    def email_file = email_file_get()
+    sh "echo > ${email_file}"
+
+    email_variable_message('WORKSPACE', env.WORKSPACE)
+    email_variable_message('JOB_NAME', env.JOB_NAME)
+    email_variable_message('NODE_NAME', env.NODE_NAME)
+    email_newline()
+    email_message("Build URL: ${env.BUILD_URL}")
+    email_message("Timestamp: ${env.BUILD_TIMESTAMP}")
+    email_newline()
+}
+
+// Add message to email
+//
+// Args:
+//   message: message string
+def email_message(String message) {
+    def email_file = email_file_get()
+    sh "echo '${message}' >> ${email_file}"
+}
+
+// Just wrapper for adding empty line
+def email_newline() {
+    email_message("")
+}
+
+// Add variable to email
+//
+// Args:
+//   name: name of variable
+//   value: value of variable
+def email_variable_message(String name, String value) {
+    email_message("${name}=${value}")
+}
+
+// Add all revisions to email text. Revision are taken
+// from the map constructed with help of teRevData.
+//
+// Args:
+//   revs: map with revisions
+def email_all_revs(Map revs) {
+    def revs_only = []
+
+    teRevData.iterate_values(revs, {
+        component, variable, value ->
+        if (variable ==~ /.*_REV$/) {
+            revs_only.add("${variable}=${value}")
+        }
+    })
+
+    email_newline()
+    email_message("Revisions:")
+    revs_only.sort().each {
+        value ->
+        email_message(value)
+    }
+}
+
+// Add stage to email
+//
+// Args:
+//   stage: stage name
+def email_stage(String stage) {
+    email_message("STAGE: ${stage}")
+}
+
+// Post email by status
+//
+// Args:
+//   status: status of build
+//   providers: recipient providers, i.e. requestor() or culprits() etc...
+def email_post(status, providers) {
+    def email_file = email_file_get()
+    def prefix = env.EMAIL_PREFIX ?: "[CI ${env.JOB_NAME}]"
+    def subject
+
+    email_newline()
+    if (status) {
+        email_message("OK")
+    } else {
+        email_message("FAIL")
+    }
+    email_newline()
+
+    subject = "${prefix} job ${currentBuild.displayName}: "
+    subject += "${currentBuild.currentResult}"
+
+    if (env.EMAIL_TRAILER) {
+        subject += " ${env.EMAIL_TRAILER}"
+    }
+
+    emailext (
+        subject: "${subject}",
+        to: env.EMAIL_TO,
+        from: env.EMAIL_FROM,
+        recipientProviders: providers,
+        attachLog: true,
+        attachmentsPattern: '**/trc-brief.html',
+        body: '${FILE, path="' + email_file + '"}'
+    )
+}

vars/teMeta.groovy
@@ -1,0 +1,87 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for generating meta_data.json which describes the run
+// and is used by Bublik.
+//
+// Requires:
+//     Pipeline Utility Steps
+
+// Get name of the file with metas
+def meta_data_fname() {
+    return 'meta_data.json'
+}
+
+// Get path to the file with metas
+def meta_data_fpath() {
+    return env.WORKSPACE + '/' + meta_data_fname()
+}
+
+// Meta generation. It saves to JSON file all contents of metas
+// variable. You can set fields in that variable to add them to
+// meta data.
+//
+// Args:
+//     ctx: pipeline context containing metas
+def meta_generate(ctx) {
+    meta_data = [
+        version: 1,
+        metas: []
+    ]
+
+    ctx.metas.each { var_name, var_value ->
+        if (var_value == null) {
+            if (ctx.containsKey(var_name)) {
+                var_value = ctx[var_name]
+            } else {
+                var_value = env[var_name]
+            }
+        }
+        if (var_value == null) {
+            return
+        }
+
+        meta_node = [
+            name: var_name,
+            value: var_value
+        ]
+
+        def meta_type = meta_name2type(var_name)
+        if (meta_type != null) {
+            meta_node.type = meta_type
+        }
+
+        meta_data.metas.add(meta_node)
+    }
+
+    writeJSON(file: meta_data_fpath(), json: meta_data, pretty: 4)
+}
+
+// Get meta node type by tail of node name
+//
+// Args:
+//     node_name: Name of node, for example TE_REV, START_TIMESTAMP
+// Return:
+//     String with type of node or null
+def meta_name2type(node_name) {
+    if (node_name.endsWith('_TIMESTAMP')) {
+        return 'timestamp'
+    }
+    if (node_name.endsWith('_COMMIT') || node_name.endsWith('_REV')) {
+        return 'revision'
+    }
+    if (node_name.endsWith('_BRANCH')) {
+        return 'branch'
+    }
+
+    return null
+}
+
+// Get current timestamp
+//
+// Return:
+//    timestamp string
+def meta_timestamp_make() {
+    def timestamp = sh(script: 'date +%FT%T%:z', returnStdout: true).trim()
+    return timestamp
+}

vars/teRevData.groovy
@@ -1,0 +1,115 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for saving and loading information about revisions and
+// repositories.
+//
+// It allows to collect all information about revisions and repositories
+// in a single map, save it to a file in JSON format which can be
+// archived as an artifact. Then another job can copy that artifact,
+// load JSON file and export values from it in environment.
+
+// Load revisions map from a file.
+//
+// Args:
+//   fpath: Path to the JSON file
+//
+// Return:
+//   Loaded map (empty if the file is absent)
+def load_revs(String fpath = "all.rev") {
+    if (fileExists(fpath)) {
+        return readJSON(file: fpath)
+    } else {
+        return [:]
+    }
+}
+
+// Try to get map with revisions from an artifact of the last successful
+// build of a given job.
+//
+// Args:
+//   job: name of the job
+//   fpath: file to get from artifacts
+//
+// Return:
+//   Loaded map (empty if the file or job is absent)
+def try_load_revs_from_job(String job = "update",
+                           String fpath = "all.rev") {
+    try {
+        copyArtifacts(projectName: job,
+                      filter: fpath, selector: lastSuccessful())
+    } catch (Exception e) {
+        return [:]
+    }
+
+    return load_revs(fpath)
+}
+
+// Store named value in a map.
+//
+// Args:
+//   revs: map
+//   component: component name (for example, "te")
+//   name: value name (for example, "TE_REV")
+//   value: value to store
+def store_value(Map revs, String component, String name, String value) {
+    if (!revs[component]) {
+        revs[component] = [:]
+    }
+
+    revs[component][name] = value
+}
+
+// Export named values to environment.
+//
+// Args:
+//   env: Environment object
+//   revs: map with values
+//   force: if true, existing environment variables should be
+//          overwritten even if they have nonempty values
+//   component: component name (if null, all components are iterated)
+//   name: value name (if null, all values will be exported)
+def export_values(env, Map revs, boolean force = false,
+                  String component = null, String name = null) {
+    revs.each {
+        comp_name, comp_vars ->
+        if (!component || component == comp_name) {
+            comp_vars.each {
+                var_name, var_value ->
+
+                if (!name || name == var_name) {
+                    if (force) {
+                        env[var_name] = var_value
+                    } else {
+                        env[var_name] = env[var_name] ?:
+                                        var_value
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Iterate all values in revisions map.
+//
+// Args:
+//   revs: map to iterate
+//   it: closure taking three arguments - component name, variable name and
+//       value
+def iterate_values(Map revs, Closure it) {
+    revs.each {
+        comp_name, comp_vars ->
+        comp_vars.each {
+            var_name, var_value -> it(comp_name, var_name, var_value)
+        }
+    }
+}
+
+// Save map with revisions to a JSON file.
+//
+// Args:
+//   revs: map
+//   fpath: file path
+def save_revs(Map revs, String fpath = "all.rev") {
+    writeJSON(file: fpath, json: revs, pretty: 4)
+}

commit a1c7e5df7ec3f4b3aeffc85bfb208108532594f5

doc: mention Parameterized Scheduler
Parameterized Scheduler plugin provides parameterizedCron() used
by teScheduledRunPipeline

Fixes: c6f6d7d7f4c8 ("vars: implement a shared library")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teScheduledRunPipeline.groovy
@@ -7,6 +7,7 @@
 // Required plugins:
 //   - Pipeline: Stage Step
 //   - Timestamper
+//   - Parameterized Scheduler
 //
 // Pipeline is expected to have the following mandatory parameters:
 //   job: which Jenkins job this pipeline should trigger