6222e565e8b06dc637a5c718b6ee324bf2dbf6ae
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2023-07-29 05:54:40 +0300

    vars: remove dpdk repo/branch autodetection
    Autodetection is not always possible; job parameters can be always used
    to specify repository and branch.
    
    Fixes: 8ed06da33860 ("vars: add pipeline and API for building DPDK")
    OL-Redmine-Id: 12984
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- vars/teDPDK.groovy
+++ vars/teDPDK.groovy
@@ -5,42 +5,6 @@
 
 import java.util.regex.Matcher
 
-// Detect DPDK repository URL and branch name by name of the job.
-// This function can set DPDK_GIT_URL and DPDK_DEF_BRANCH in
-// the context.
-//
-// Args:
-//   ctx: pipeline context
-//   name: job name
-def detect_url_branch(ctx, String name) {
-    if (!ctx.DPDK_GIT_URL) {
-        switch (name - ~"^.*/") {
-            case 'dpdk':
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk'
-                break
-            case ~/^dpdk-stable-.*/:
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk-stable'
-                break
-            case 'dpdk-next-net':
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/next/dpdk-next-net'
-                break
-            case 'dpdk-next-virtio':
-                ctx.DPDK_GIT_URL='git://dpdk.org/next/dpdk-next-virtio'
-                break
-        }
-    }
-
-    if (!ctx.DPDK_DEF_BRANCH) {
-        switch (name - ~"^.*/") {
-            case ~/^dpdk-stable-(.*)/:
-                ctx.DPDK_DEF_BRANCH = Matcher.lastMatcher[1]
-                break
-            default:
-                ctx.DPDK_DEF_BRANCH = 'main'
-        }
-    }
-}
-
 // Clone DPDK repository.
 //
 // Args:

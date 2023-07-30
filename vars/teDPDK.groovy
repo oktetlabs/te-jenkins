oktetlabs/te-jenkins

commit 8ed06da33860d950c59292f700eabc9e7ea49594

vars: add pipeline and API for building DPDK
It will be used by test suites which test DPDK.
Code for dpdk pipeline and helper scripts is taken from ts-jenkins
repository.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

pipelines/dpdk
@@ -1,0 +1,123 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2022-2023 OKTET Labs Ltd. All rights reserved.
+//
+// Pipeline for building DPDK (to check whether a given revision
+// of a given repository is buildable).
+//
+// Pipeline is run on a node with label 'main'. It searches for
+// all nodes having label specified in 'label' parameter (by
+// default - 'build-dpdk') and builds DPDK on all of them.
+
+library('teLib') _
+
+def ctx = teCommon.create_pipeline_ctx(env, params)
+
+// Send email about build status.
+// It is assumed that TE_EMAIL_TO_DPDK in environment
+// contains destination address(es).
+//
+// Args:
+//   ctx: pipeline context
+//   status: (true or false, meaning success or failure)
+//   trailer: optional trailer for email subject
+def post_email(ctx, Boolean status, String trailer = null) {
+    teEmail.email_add_to_by_ids(ctx, "DPDK")
+    teEmail.email_set_prefix(ctx, "[CI DPDK] ${ctx.job_last_name}")
+
+    if (trailer) {
+        teEmail.email_set_trailer(ctx, trailer)
+    }
+
+    teEmail.email_start()
+    teEmail.email_post(ctx, status, null)
+}
+
+pipeline {
+    parameters {
+        string name: 'label', trim: true,
+               description: 'Nodes to build on',
+               defaultValue: 'build-dpdk'
+        string name: 'dpdk_repo', trim: true,
+               description: 'Git repository URL (sticky default)',
+               defaultValue: "${params.dpdk_repo}"
+        string name: 'dpdk_branch', trim: true,
+               description: 'Git branch (sticky default)',
+               defaultValue: "${params.dpdk_branch}"
+        string name: 'dpdk_rev', trim: true,
+               description: 'Revision to build',
+               defaultValue: ''
+    }
+
+    options {
+        buildDiscarder(logRotator(daysToKeepStr:'15', numToKeepStr:'20',
+                                  artifactNumToKeepStr: '100'))
+        disableConcurrentBuilds()
+        timestamps()
+        checkoutToSubdirectory('te-jenkins')
+        copyArtifactPermission('*')
+    }
+
+    triggers {
+        pollSCM('H * * * *')
+    }
+
+    agent {
+        label "main"
+    }
+
+    stages {
+
+        stage("Snapshot") {
+            steps {
+                script {
+                    ctx.job_last_name = env.JOB_NAME.replaceAll(/^.*\//, '')
+                    teDPDK.detect_url_branch(ctx, ctx.job_last_name)
+                    teDPDK.checkout(ctx: ctx)
+                }
+            }
+        }
+
+        stage("Build") {
+            steps {
+                script {
+                    def builders = [:]
+                    def nodes = teCommon.getNodes(params.label)
+
+                    for (build_node in nodes) {
+                        builders[build_node] = {
+                            node(build_node) {
+                                dir('te-jenkins') {
+                                    checkout scm
+                                }
+
+                                teDPDK.checkout(ctx: ctx, do_poll: false)
+                                teDPDK.build()
+                            }
+                        }
+                    }
+                    parallel builders
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                ctx.revdata_archive()
+            }
+        }
+
+        unsuccessful {
+            script {
+                post_email(ctx, false, 'unsuccessful')
+            }
+        }
+
+        fixed {
+            script {
+                post_email(ctx, true, 'fixed')
+            }
+        }
+    }
+}

vars/teDPDK.groovy
@@ -1,0 +1,91 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 OKTET Labs Ltd. All rights reserved.
+//
+// Helper for working with DPDK repository.
+
+import java.util.regex.Matcher
+
+// Detect DPDK repository URL and branch name by name of the job.
+// This function can set DPDK_GIT_URL and DPDK_DEF_BRANCH in
+// the context.
+//
+// Args:
+//   ctx: pipeline context
+//   name: job name
+def detect_url_branch(ctx, String name) {
+    if (!ctx.DPDK_GIT_URL) {
+        switch (name - ~"^.*/") {
+            case 'dpdk':
+                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk'
+                break
+            case ~/^dpdk-stable-.*/:
+                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk-stable'
+                break
+            case 'dpdk-next-net':
+                ctx.DPDK_GIT_URL = 'git://dpdk.org/next/dpdk-next-net'
+                break
+            case 'dpdk-next-virtio':
+                ctx.DPDK_GIT_URL='git://dpdk.org/next/dpdk-next-virtio'
+                break
+        }
+    }
+
+    if (!ctx.DPDK_DEF_BRANCH) {
+        switch (name - ~"^.*/") {
+            case ~/^dpdk-stable-(.*)/:
+                ctx.DPDK_DEF_BRANCH = Matcher.lastMatcher[1]
+                break
+            default:
+                ctx.DPDK_DEF_BRANCH = 'main'
+        }
+    }
+}
+
+// Clone DPDK repository.
+//
+// Args:
+//   map of arguments (see the second variant of this function for
+//   descriptions)
+//
+// Return:
+//   List of checkout details as per GitSCM module.
+def checkout(Map args) {
+    return teRun.generic_checkout(ctx: args.ctx, component: 'dpdk',
+                                  url: args.url, revision: args.revision,
+                                  target: args.target ?: 'dpdk',
+                                  do_poll: args.do_poll == null ?
+                                                    true : args.do_poll)
+}
+
+// Clone DPDK repository.
+//
+// Args:
+//   ctx: pipeline context
+//   url: repository URL
+//   revision: branch, tag or SHA-1
+//   target: target directory
+//   do_poll: if true, this checkout should be taken into account by
+//            pollSCM() trigger
+//
+// Return:
+//   List of checkout details as per GitSCM module.
+def checkout(ctx, String url = null, String revision = null,
+             String target = 'dpdk', Boolean do_poll = true) {
+
+    return teDPDK.checkout(
+                      ctx: ctx, url: url, revision: revision,
+                      target: target, do_poll: do_poll)
+}
+
+// Build DPDK.
+//
+// Args:
+//   target: directory with DPDK sources
+//   prefix: installation prefix
+def build(String target = 'dpdk', String prefix = '') {
+    dir(target) {
+        sh """
+           "${env.WORKSPACE}"/te-jenkins/scripts/dpdk-build.sh ${prefix}
+           """
+    }
+}

scripts/dpdk-build.sh
@@ -1,0 +1,36 @@
+#!/bin/bash
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2022-2023 OKTET Labs Ltd. All rights reserved.
+#
+# Script used to build DPDK.
+
+PREFIX="$1"
+
+echo "Building dpdk on host $(hostname)"
+
+set -e
+
+# Load configuration and helper functions
+MYDIR="$(cd "$(dirname "$(which "$0")")" ; pwd -P)"
+source "${MYDIR}"/functions
+
+
+function meson_build() {
+    local build="$1" ; shift
+    local meson_args=()
+
+    # Disable kernel modules build since it requires extra rights to install
+    meson_args+=(-Denable_kmods=false)
+
+    test -z "${PREFIX}" || meson_args+=(--prefix "${PREFIX}" --libdir lib)
+
+    meson "${meson_args[@]}" "${build}" || return 1
+    ninja_build -C "${build}" || return 1
+    if test -n "${PREFIX}" ; then
+        ninja_build install -C "${build}" || return 1
+    fi
+    ln -snf "${build}" build
+    return 0
+}
+
+meson_build meson_build

scripts/functions
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2022-2023 OKTET Labs Ltd. All rights reserved.
+#
+# Helper functions for scripts.
+
+# Helper function to split ninja output into stdout and stderr
+function ninja_filter () {
+	local l n
+	while read -r l; do
+		n=${l#\[ninja\]}
+		test "$l" != "$n" && printf "%s\n" "$n" || printf "%s\n" "$l" >&2
+	done
+}
+
+# Helper function to run ninja taking ninja-build vs ninja difference into
+# account and splitting stdout and stderr streams
+function ninja_build () {
+	local ninja
+
+	which ninja-build >/dev/null 2>&1 && ninja=ninja-build || ninja=ninja
+	(
+		set -o pipefail
+		NINJA_STATUS="[ninja][%f/%t] " ${ninja} "$@" | ninja_filter
+	)
+}

vars/teCommon.groovy
@@ -21,6 +21,7 @@ def create_pipeline_ctx(env, params) {
     dlg.teEmail = teEmail
     dlg.teMeta = teMeta
     dlg.teRevData = teRevData
+    dlg.teDPDK = teDPDK
     dlg.teCommon = teCommon
 
     dlg.env = env
@@ -35,4 +36,23 @@ def create_pipeline_ctx(env, params) {
     teRevData.init_ctx(dlg)
 
     return dlg
+}
+
+// Get all nodes having specific label.
+//
+// Args:
+//   label: label to look for
+//
+// Return: list of node names
+@NonCPS
+def getNodes(String label) {
+    def nodes = jenkins.model.Jenkins.instance.nodes
+
+    def filteredNodes = nodes.findAll { node ->
+        node.labelString.contains("${label}")
+    }
+
+    return filteredNodes.collect { node ->
+        node.name
+    }
 }

commit 6222e565e8b06dc637a5c718b6ee324bf2dbf6ae

vars: remove dpdk repo/branch autodetection
Autodetection is not always possible; job parameters can be always used
to specify repository and branch.

Fixes: 8ed06da33860 ("vars: add pipeline and API for building DPDK")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

pipelines/dpdk
@@ -71,7 +71,6 @@ pipeline {
             steps {
                 script {
                     ctx.job_last_name = env.JOB_NAME.replaceAll(/^.*\//, '')
-                    teDPDK.detect_url_branch(ctx, ctx.job_last_name)
                     teDPDK.checkout(ctx: ctx)
                 }
             }

vars/teDPDK.groovy
@@ -5,42 +5,6 @@
 
 import java.util.regex.Matcher
 
-// Detect DPDK repository URL and branch name by name of the job.
-// This function can set DPDK_GIT_URL and DPDK_DEF_BRANCH in
-// the context.
-//
-// Args:
-//   ctx: pipeline context
-//   name: job name
-def detect_url_branch(ctx, String name) {
-    if (!ctx.DPDK_GIT_URL) {
-        switch (name - ~"^.*/") {
-            case 'dpdk':
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk'
-                break
-            case ~/^dpdk-stable-.*/:
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/dpdk-stable'
-                break
-            case 'dpdk-next-net':
-                ctx.DPDK_GIT_URL = 'git://dpdk.org/next/dpdk-next-net'
-                break
-            case 'dpdk-next-virtio':
-                ctx.DPDK_GIT_URL='git://dpdk.org/next/dpdk-next-virtio'
-                break
-        }
-    }
-
-    if (!ctx.DPDK_DEF_BRANCH) {
-        switch (name - ~"^.*/") {
-            case ~/^dpdk-stable-(.*)/:
-                ctx.DPDK_DEF_BRANCH = Matcher.lastMatcher[1]
-                break
-            default:
-                ctx.DPDK_DEF_BRANCH = 'main'
-        }
-    }
-}
-
 // Clone DPDK repository.
 //
 // Args: