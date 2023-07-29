oktetlabs/te-jenkins

commit 0aaa8b37ccc63f471caadab647db556c717d6113

vars: improve saving information about repository
Use data returned by 'checkout' step instead of calling git
via 'sh' step, it should be faster. Store information about
branch, it is useful for debugging. Use _GIT_URL suffix instead
of _URL for repository URL - it is what get_url() expects when
trying to determine URL to clone.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRun.groovy
@@ -129,13 +129,21 @@ def generic_checkout(ctx, String component, String url = null,
         scm_vars = git_checkout(repo, rev)
 
         ctx["${var_prefix}SRC"] = pwd()
-        ctx.metas["${var_prefix}REV"] = git_get_rev()
+        ctx.metas["${var_prefix}REV"] = scm_vars.GIT_COMMIT
     }
 
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}URL", repo)
+                          "${var_prefix}GIT_URL", repo)
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}REV", ctx.metas["${var_prefix}REV"])
+                          "${var_prefix}REV", scm_vars.GIT_COMMIT)
+
+    // 'detached' means that repository was checked out to a specific
+    // changeset, not a branch. Hopefully nobody calls a real
+    // branch 'detached'.
+    if (scm_vars.GIT_BRANCH != 'detached') {
+        teRevData.store_value(ctx.all_revs, component,
+                              "${var_prefix}BRANCH", scm_vars.GIT_BRANCH)
+    }
 
     return scm_vars
 }

commit 1acf1b9515f291b5d3007c54b783fa689e33b538

vars: keep actual branch name in artifacts
If branch name is inherited from upstream job artifact but
different revision is used, clean branch name.

Fixes: 0aaa8b37ccc6 ("vars: improve saving information about repository")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRevData.groovy
@@ -60,6 +60,36 @@ def store_value(Map revs, String component, String name, String value) {
     revs[component][name] = value
 }
 
+// Get named value from a map.
+//
+// Args:
+//   revs: map
+//   component: component name (for example, "te")
+//   name: value name (for example, "TE_REV")
+def get_value(Map revs, String component, String name) {
+    if (!revs[component]) {
+        return null
+    }
+
+    return revs[component][name]
+}
+
+// Remove named value from a map.
+//
+// Args:
+//   revs: map
+//   component: component name (for example, "te")
+//   name: value name (for example, "TE_REV")
+def remove_value(Map revs, String component, String name) {
+    if (!revs[component]) {
+        return
+    }
+
+    if (revs[component].containsKey(name)) {
+        revs[component].remove(name)
+    }
+}
+
 // Export named values to a map (or map-like object such as env).
 //
 // Args:
@@ -115,9 +145,19 @@ def init_ctx(ctx) {
     // Revisions data storage.
     ctx.all_revs = [:]
 
+    // Get named value for a specific component.
+    ctx.revdata_get = { component, name ->
+        return get_value(ctx.all_revs, component, name)
+    }
+
     // Set named value for a specific component.
     ctx.revdata_set = { component, name, value ->
         store_value(ctx.all_revs, component, name, value)
+    }
+
+    // Remove named value for a specific component.
+    ctx.revdata_del = { component, name ->
+        remove_value(ctx.all_revs, component, name)
     }
 
     // Archive revisions data in an artifact.

vars/teRun.groovy
@@ -121,6 +121,7 @@ def generic_checkout(ctx, String component, String url = null,
     String rev = revision ?: get_rev(ctx, component)
     String repo = url ?: get_url(ctx, component)
     String var_prefix
+    String prev_rev
 
     if (!repo) {
         error "Repository URL is not defined for ${component}"
@@ -136,7 +137,16 @@ def generic_checkout(ctx, String component, String url = null,
     }
 
     ctx.revdata_set(component, "${var_prefix}GIT_URL", repo)
+
+    prev_rev = ctx.revdata_get(component, "${var_prefix}REV")
     ctx.revdata_set(component, "${var_prefix}REV", scm_vars.GIT_COMMIT)
+
+    if (prev_rev != scm_vars.GIT_COMMIT) {
+        // Clean branch if it was inherited from artifacts of upstream
+        // job and revision was not taken from there.
+
+        ctx.revdata_del(component, "${var_prefix}BRANCH" )
+    }
 
     // 'detached' means that repository was checked out to a specific
     // changeset, not a branch. Hopefully nobody calls a real