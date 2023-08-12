oktetlabs/te-jenkins

commit 0aaa8b37ccc63f471caadab647db556c717d6113

vars: improve saving information about repository
Use data returned by 'checkout' step instead of calling git
via 'sh' step, it should be faster. Store information about
branch, it is useful for debugging. Use _GIT_URL suffix instead
of _URL for repository URL - it is what get_url() expects when
trying to determine URL to clone.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRun.groovy
@@ -129,13 +129,21 @@ def generic_checkout(ctx, String component, String url = null,
         scm_vars = git_checkout(repo, rev)
 
         ctx["${var_prefix}SRC"] = pwd()
-        ctx.metas["${var_prefix}REV"] = git_get_rev()
+        ctx.metas["${var_prefix}REV"] = scm_vars.GIT_COMMIT
     }
 
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}URL", repo)
+                          "${var_prefix}GIT_URL", repo)
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}REV", ctx.metas["${var_prefix}REV"])
+                          "${var_prefix}REV", scm_vars.GIT_COMMIT)
+
+    // 'detached' means that repository was checked out to a specific
+    // changeset, not a branch. Hopefully nobody calls a real
+    // branch 'detached'.
+    if (scm_vars.GIT_BRANCH != 'detached') {
+        teRevData.store_value(ctx.all_revs, component,
+                              "${var_prefix}BRANCH", scm_vars.GIT_BRANCH)
+    }
 
     return scm_vars
 }

commit 5364798ce08961fdff0e1c1fbf3a6d5219d32c0f

vars: fix obtaining current revision
scm_vars.GIT_COMMIT cannot be relied upon when we check out to a
specific revision, not to some branch. Revert to using git_get_rev().

Fixes: 0aaa8b37ccc6 ("vars: improve saving information about repository")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRun.groovy
@@ -119,6 +119,7 @@ def generic_checkout(ctx, String component, String url = null,
 
     def scm_vars
     String rev = revision ?: get_rev(ctx, component)
+    String rev_got
     String repo = url ?: get_url(ctx, component)
     String var_prefix
     String prev_rev
@@ -134,14 +135,19 @@ def generic_checkout(ctx, String component, String url = null,
         scm_vars = git_checkout(repo, rev, do_poll)
 
         ctx["${var_prefix}SRC"] = pwd()
+
+        // scm_vars.GIT_COMMIT can report wrong revision (of default
+        // branch) when we check out specific revision and
+        // move to "detached" state.
+        rev_got = git_get_rev()
     }
 
     ctx.revdata_set(component, "${var_prefix}GIT_URL", repo)
 
     prev_rev = ctx.revdata_get(component, "${var_prefix}REV")
-    ctx.revdata_set(component, "${var_prefix}REV", scm_vars.GIT_COMMIT)
+    ctx.revdata_set(component, "${var_prefix}REV", rev_got)
 
-    if (prev_rev != scm_vars.GIT_COMMIT) {
+    if (prev_rev != rev_got) {
         // Clean branch if it was inherited from artifacts of upstream
         // job and revision was not taken from there.
 
@@ -157,7 +163,7 @@ def generic_checkout(ctx, String component, String url = null,
     }
 
     ctx.metas["${var_prefix}GIT_URL"] = repo
-    ctx.metas["${var_prefix}REV"] = scm_vars.GIT_COMMIT
+    ctx.metas["${var_prefix}REV"] = rev_got
 
     if ((branch = ctx.revdata_get(component, "${var_prefix}BRANCH"))) {
         ctx.metas["${var_prefix}BRANCH"] = branch