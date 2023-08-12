oktetlabs/te-jenkins

commit 0aaa8b37ccc63f471caadab647db556c717d6113

vars: improve saving information about repository
Use data returned by 'checkout' step instead of calling git
via 'sh' step, it should be faster. Store information about
branch, it is useful for debugging. Use _GIT_URL suffix instead
of _URL for repository URL - it is what get_url() expects when
trying to determine URL to clone.

OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRun.groovy
@@ -129,13 +129,21 @@ def generic_checkout(ctx, String component, String url = null,
         scm_vars = git_checkout(repo, rev)
 
         ctx["${var_prefix}SRC"] = pwd()
-        ctx.metas["${var_prefix}REV"] = git_get_rev()
+        ctx.metas["${var_prefix}REV"] = scm_vars.GIT_COMMIT
     }
 
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}URL", repo)
+                          "${var_prefix}GIT_URL", repo)
     teRevData.store_value(ctx.all_revs, component,
-                          "${var_prefix}REV", ctx.metas["${var_prefix}REV"])
+                          "${var_prefix}REV", scm_vars.GIT_COMMIT)
+
+    // 'detached' means that repository was checked out to a specific
+    // changeset, not a branch. Hopefully nobody calls a real
+    // branch 'detached'.
+    if (scm_vars.GIT_BRANCH != 'detached') {
+        teRevData.store_value(ctx.all_revs, component,
+                              "${var_prefix}BRANCH", scm_vars.GIT_BRANCH)
+    }
 
     return scm_vars
 }

commit d758a497f2e60fb3ac7b1e440770f738665e3cc2

vars: check whether branch is available
It appears checkout step can return result which does not contain
GIT_BRANCH at all. Code in generic_checkout() should handle it.

Fixes: 0aaa8b37ccc6 ("vars: improve saving information about repository")
OL-Redmine-Id: 12984
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

vars/teRun.groovy
@@ -157,7 +157,7 @@ def generic_checkout(ctx, String component, String url = null,
     // 'detached' means that repository was checked out to a specific
     // changeset, not a branch. Hopefully nobody calls a real
     // branch 'detached'.
-    if (scm_vars.GIT_BRANCH != 'detached') {
+    if (scm_vars.GIT_BRANCH && scm_vars.GIT_BRANCH != 'detached') {
         ctx.revdata_set(component, "${var_prefix}BRANCH",
                         scm_vars.GIT_BRANCH - ~/^\/?origin\//)
     }